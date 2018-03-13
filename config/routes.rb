9d53e57daedd97dc37e6b3b6b82748fb3b3e9640
Ming <minghaozhou123@gmail.com>
2018-03-13 17:14:20 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'devise/sessions'
   }
+  
   devise_scope :user do
     authenticated :user do
-      root 'users#index', as: :authenticated_root
+      root 'game#index', as: :authenticated_root
     end
 
     resources :users
-
+    resources :game
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
